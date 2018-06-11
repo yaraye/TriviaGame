b20416b7e70b77105ea8fe7120b10a81e31bac65
Yaraye <yaraye@gmail.com>
2018-06-11 00:54:39 -0400

    new-code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,143 +1,105 @@
+
 //creat a team-Fun Trivia Questions
-var funTriviaQuestions=["What nation was bounced from the Organisation of American States in 1962?",
-"What continent has the fewest flowering plants?",
-"What was the first planet to be discovered using the telescope, in 1781?"]
-//-------------------------------------------------------------------------
-// Variable showQuestion will hold the setInterval when we start the slideshow
-var showQuestion;
-
-function questionOne(){
-    $("#chose-1").html(funTriviaQuestions[count]);
-    console.log("5 seconds left");
-}
-function questiontwo(){
-    $("#chose-2").html(funTriviaQuestions[count]);
-    console.log("5 seconds left");
-}
-//count will keep track of the index of displayed questions
-var count=0;
-//jquery to start the slideshow
-$("start").click(startSlideshow);
-
-//--------------------------------------------------------------------------
-
-
-function nextQuestion()
-{
-    count ++;
-    questiontwo();
-    setTimeout(questionOne,1000);
-//stop when question ends
-    if (count ===funTriviaQuestions.length)
+var funTriviaQuestions=[
     {
-        count=0;
-    }
-}
-//slideshow function
-function startSlideshow()
-{
-   showImage=setInterval(questiontwo,1000);
-}
-//stop slideshow
-function stopSlideshow()
-{
-    clearInterval(showQuestion);
-}
-//question start to show
-questionOne();
-//--------------------------------------------------------------------------------
-// //setting the timer
- 
-    //  Set our number counter to 5.
-    var number = 5;
-    //  Variable that will hold our interval ID when we execute
-    var intervalId;
-
-//timer function
-function timeCounter()
-{
-    document.getElementById("timer").innerHTML = "Time Remaining:" + number +  "Seconds";
-}
-
-    //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
-    function run() {
-      clearInterval(intervalId);
-      intervalId = setInterval(decrement, 1000);
-      startSlideshow();
-
-    }
-    //  The decrement function.
-    function decrement() {
-      //  Decrease number by one.
-      number--;
-      //  Show the number in the #show-number tag.
-      timeCounter()
-
-      //  Once number hits zero...
-      if (number === 0) {
-        //  ...run the stop function.
+    question: "What nation was bounced from the Organisation of American States in 1962?",
+    answers:{
+        a:"Cuba",
+        b:"Canada",
+        c:"Mixico",
+    },
+    correctAnswer: "a"
+    },
+    {
+    question:"What continent has the fewest flowering plants?",
+    answers:{
+        a:"Afirca",
+        b:"Antarctica",
+        c:"Asia",
+    },
+    correctAnswer: "b"
+    },
+    {
+    question:"What was the first planet to be discovered using the telescope, in 1781?",
+    answers:{
+        a:"Uranus",
+        b:"Venus",
+        c:"Mercury",
+    },
+    correctAnswer: "b"
+    },
+    ];
+    var triviachoice=["Cuba","Canda","Mixico"];
     
-        stop();
-        clearInterval(intervalId);
-        
-
-      }
-    }
-    //  The stop function
-    function stop() {
-      //  Clears our intervalId
-      //  We just pass the name of the interval
-      //  to the clearInterval function.
-      clearInterval(intervalId);
-      startSlideshow();
-        
-    }
-    //  Execute the run function.
-    run();
-//----------------------------------------------------------------
-//setting the question based on time
-
-setTimeout(fiveSeconds, 1000 * 5);
-setTimeout(tenSeconds, 1000 * 10);
-function questionOne(){
-    $("#chose-1").html(funTriviaQuestions[count]);
-    //append("<p>What nation was bounced from the Organisation of American States in 1962? </p>");
-    console.log("5 seconds left");
-}
-function fiveSeconds() {
-
-    // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
-    // console log 10 seconds left
-   
-    questionOne()
-  }
-  function tenSeconds() {
-
-    // in the element with an id of time-left add an h2 saying About 10 Seconds Left!
-    // console log 10 seconds left
+    //finding selected answers
+    var userAnswer = (answersContainer);
     
-    $("#chose-2").append("<p>What continent has the fewest flowering plants? </p>");
-    console.log("10 seconds left");
-  }
+//referance for html tags
+var traviaContainer= document.getElementById("triviaQuestions");
+var timerContainer= document.getElementById("timer");
+var answersContainer= document.getElementById("results");
 
 
+//show the result and put it all togeher
+
+function buildTriva(){
+    var output=[];
+    funTriviaQuestions.forEach (function
+        (currentQuestion, questionNumber)
+{
+    var answers=[];
+    for (i=0; i<funTriviaQuestions.answers; i++){
+        answers.push(
+            "<label>  <input type='radio' name='question${questionNumber}' value = '${letter}'> ${letter}: ${funTriviaQuestions.answers[letter]}</label>"
+      );
+    }
+    //question ans answer in the output
+    output.push (
+    '<div class="question"> ${funTriviaQuestions.question} </div> <div class="answers"> ${answers.join(" ")} </div>'
+    );
+}
+    );
+}
+//displaying the answer
+function showAnswer(){
+    var answersContainer=funTriviaQuestions.querySelectorAll('.answer');
+
+  // for each question...
+  funTriviaQuestions.forEach( function(currentQuestion, questionNumber){
+
+    // find selected answer
+   var answersContainer = answerContainers[questionNumber];
+   var selector = 'input[name=question'+questionNumber+']:checked';
+   var userAnswer = (answersContainer.querySelector(selector) || {}).value;
+
+    // if answer is correct
+    if(userAnswer===funTriviaQuestions.correctAnswer){
+      // add to the number of correct answers
+      numCorrect++;
+    }
+else{
+    alert("make a choice");
+}
+  
+});
+}
+
+//display question 
+buildTriva();
 
 
-// You'll create a trivia game that shows only one question until the player answers it or
-// their time runs out.
+//evaluating answer with results
+//if answer is correct
+
+if(userAnswers===currentQuestion.correctAnswer){
+    // add to the number of correct answers
+    numCorrect++;
+    alert('yea');
+}else
+{
+    alert("next question");
+}
 
 
-
-//  If the player selects the correct answer, show a screen congratulating them for choosing the right option. 
-//After a few seconds, display the next question -- do this without user input.
-
-// * The scenario is similar for wrong answers and time-outs.
-
-// If the player runs out of time, tell the player that time's up and display the correct answer. 
-//Wait a few seconds, then show the next question.
-
-//If the player chooses the wrong answer, tell the player they selected the wrong option and 
-//then display the correct answer. Wait a few seconds, then show the next question.
-
-//On the final screen, show the number of correct answers, incorrect answers, 
-//and an option to restart the game (without reloading the page).
+//on submit show result
+submitButton.addEventListener("click", showAnswer)
