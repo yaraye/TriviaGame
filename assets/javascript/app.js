8e41bc6be9945df213ab1bed925e0d69bf55d1de
Yaraye <yaraye@gmail.com>
2018-06-14 16:30:23 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,105 +1,88 @@
 
-//creat a team-Fun Trivia Questions
-var funTriviaQuestions=[
-    {
-    question: "What nation was bounced from the Organisation of American States in 1962?",
-    answers:{
-        a:"Cuba",
-        b:"Canada",
-        c:"Mixico",
-    },
-    correctAnswer: "a"
-    },
-    {
-    question:"What continent has the fewest flowering plants?",
-    answers:{
-        a:"Afirca",
-        b:"Antarctica",
-        c:"Asia",
-    },
-    correctAnswer: "b"
-    },
-    {
-    question:"What was the first planet to be discovered using the telescope, in 1781?",
-    answers:{
-        a:"Uranus",
-        b:"Venus",
-        c:"Mercury",
-    },
-    correctAnswer: "b"
-    },
-    ];
-    var triviachoice=["Cuba","Canda","Mixico"];
-    
-    //finding selected answers
-    var userAnswer = (answersContainer);
-    
-//referance for html tags
-var traviaContainer= document.getElementById("triviaQuestions");
-var timerContainer= document.getElementById("timer");
-var answersContainer= document.getElementById("results");
 
+function Question(text, choices, answer){
+  this.text=text;
+  this.choices=choices;
+  this.answer=answer;
 
-//show the result and put it all togeher
-
-function buildTriva(){
-    var output=[];
-    funTriviaQuestions.forEach (function
-        (currentQuestion, questionNumber)
-{
-    var answers=[];
-    for (i=0; i<funTriviaQuestions.answers; i++){
-        answers.push(
-            "<label>  <input type='radio' name='question${questionNumber}' value = '${letter}'> ${letter}: ${funTriviaQuestions.answers[letter]}</label>"
-      );
-    }
-    //question ans answer in the output
-    output.push (
-    '<div class="question"> ${funTriviaQuestions.question} </div> <div class="answers"> ${answers.join(" ")} </div>'
-    );
-}
-    );
-}
-//displaying the answer
-function showAnswer(){
-    var answersContainer=funTriviaQuestions.querySelectorAll('.answer');
-
-  // for each question...
-  funTriviaQuestions.forEach( function(currentQuestion, questionNumber){
-
-    // find selected answer
-   var answersContainer = answerContainers[questionNumber];
-   var selector = 'input[name=question'+questionNumber+']:checked';
-   var userAnswer = (answersContainer.querySelector(selector) || {}).value;
-
-    // if answer is correct
-    if(userAnswer===funTriviaQuestions.correctAnswer){
-      // add to the number of correct answers
-      numCorrect++;
-    }
-else{
-    alert("make a choice");
-}
-  
-});
 }
 
-//display question 
-buildTriva();
-
-
-//evaluating answer with results
-//if answer is correct
-
-if(userAnswers===currentQuestion.correctAnswer){
-    // add to the number of correct answers
-    numCorrect++;
-    alert('yea');
-}else
-{
-    alert("next question");
+//answer corrector not
+Question.prototype.correctAnswer=function(choice){
+  return this.choice===this.answer;
 }
 
+function Quiz(questions){
+  this.score=0;
+  this.questions=questions;
+  this.questionIndex=0;
+}
 
-//on submit show result
-submitButton.addEventListener("click", showAnswer)
+Quiz.prototype.getQuestionIndex= function (){
+  return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.isEnded= function() {
+  return this.questions.length===this.questionIndex;
+
+}
+Quiz.prototype.guess =function(answer){
+
+  //check if answer is correct
+  if(this .getQuestionIndex().correctAnswer(answer)){
+      this.score++;
+  }
+  this.questionIndex++;
+}
+function populate (){
+  if(quiz.isEnded()){
+       showScores();
+  }
+  else {
+      //show question();
+      var element= document.getElementById("question");
+      element.innerHTML=quiz.getQuestionIndex().text;
+
+      //show choice
+      var choices=quiz.getQuestionIndex().choices;
+      for(var i=0; i<choices.length; i++){
+          var element= document. getElementById("choice" + i);
+          element.innerHTML=choices[i];
+          guess("btn" + i, choices[i]);
+      }
+      showProgress();
+  }
+};
+function guess(id,guess) {
+  var button= document.getElementById(id);
+  button.onclick=function(){
+      quiz.guess(guess);
+      populate();
+
+  }
+
+};
+function showProgress(){
+  var currentQuestionNumber= quiz.questionIndex + 1;
+  var element = document.getElementById("progress");
+  element.innerHTML= "Question" + currentQuestionNumber + "of" + quiz.questions.length;
+
+}
+function showScores (){
+  var gameOverhtml='<h1>Result</h1>'
+  gameOverhtml+="<h2 id='score'> Your scores:"+ quiz.score+"</h2>";
+  var element=document.getElementById("quiz");
+  element.innerHTML=gameOverhtml;
+
+};
+var questions=[
+  new Question("What nation was bounced from the Organisation of American States in 1962?", ["Cuba","Canada","Mixico","Gana"],"Cuba"),
+  new Question("2What nation was bounced from the Organisation of American States in 1962?",["Cuba","Canada","Mixico","Gana"],"Canda"),
+  new Question("3What nation was bounced from the Organisation of American States in 1962?",["Cuba","Canada","Mixico","Gana"],"Mixico")
+];
+
+//
+var quiz= new Quiz(questions);
+
+populate();
+
