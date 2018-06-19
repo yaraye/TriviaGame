8ca9beb2b4612067d0d1b5da69e9f1446aac9d1f
Yaraye <yaraye@gmail.com>
2018-06-19 03:43:07 -0400

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,193 +1,142 @@
 
+var funTriviaQuestions=[{
+    question: 'What nation was bounced from the Organisation of American States in 1962?',
+    answers:["Cuba", "Canada","Mixico","Gana"],
+    correctAnswer:"Cuba",
+    image:'assets/'
+},{
+    question:'What continent has the fewest flowering plants?',
+    answers:["Afirca", "Antarctica","Asia","Australia"],
+    correctAnswer:"Antarctica",
+   image:"assets/images/trivia1.jpn.jpg"
+},{
+    question:'What was the first planet to be discovered using the telescope, in 1781?',
+    answers:["Uranus","Venus","Mercury","Earth"],
+    correctAnswer:"Uranus",
+    image:"assets/images/trivia1.jpn.jpg"
+},{
+    question:'What country saw a world record 315 million voters turn out for elections on May 20, 1991?',
+    answers:["India","Canada","Mixico","Ethiopia"],
+    correctAnswer:"India",
+    image:"assets/images/trivia1.jpn.jpg"   
+},{
+    question:'What national holiday in Mexico has picnickers munching chocolate coffins and sugar skulls?',
+    answers:["The Day of the Dead","Walking Dead","Dead People","Day of the Dead"],
+    correctAnswer:"The Day of the Dead",
+    image:"assets/images/trivia1.jpn.jpg"  
+}];
 
-$(function() {
-  $('.buttons').hide();
-});
 
-// Count will keep track of the index of the currently displaying picture.
-
-var count = 0;
-
-//start button 
-    $('#start').on('click', function(){
-    $(this).hide();
-    countdown ();
-$(function() {
-  $('.buttons').show();
-});
+$('#start').on('click',function(){
+    $('#start').hide();
     populate();
-   
-    });
-   
-    //set timer
-function countdown(){
-	seconds = 5;
-	$('#timer').html('<h4>Time Remaining: ' + seconds +' '+ 'Seconds </h4>');
-	answer = true;
-	//sets timer to go down
-    time = setInterval(showCountdown, 1000);
-   
 
-}
-function showCountdown(){
-	seconds--;
-	$('#timer').html('<h4>Time Remaining: ' + seconds +' '+ 'Seconds </h4>');
-	if(seconds < 1){
-        clearInterval(time);
-        console.log(seconds);
-        populate();
-        quiz.guess();
-	}
-};
+    $(document).on('click','.clickedAnswer', function(e)
+    {
+        clicked(e);  
+    }
+)
+})
 
+$(document).on('click','#reset', function()
+{
+    reset();
+})
 
-function funTriviaGameQuestion(text, choices, answer){
-  this.text=text;
-  this.choices=choices;
-  this.answer=answer;
-}
+var funTriviaQuestions=funTriviaQuestions;
+  var  currentQuestion=0;
+ var   counter=5;
+  var  correct=0;
+ var   incorrect=0;
+  var  unanswered=0;
 
-//answer corrector not
-funTriviaGameQuestion.prototype.correctAnswer=function(choices){
-  return this.choices===this.answer;
-  //on submit show result
-onclick.addEventListener("click", showAnswer)
-}
-
-function Quiz(questions){
-  this.score=0;
-  this.questions=questions;
-  this.questionIndex=0;
-}
-
-Quiz.prototype.getQuestionIndex= function (){
-  return this.questions[this.questionIndex];
-}
-
-Quiz.prototype.isEnded= function() {
-  return this.questions.length===this.questionIndex;
-
-
-}
-Quiz.prototype.guess =function(answer){
-console.log(this.questions[this.questionIndex].answer);
-
-  //check if answer is correct
-  if(this.questions[this.questionIndex].answer===answer){
-      this.score++; 
-      console.log("correctAnswer");
-  }
-  
-  this.questionIndex++;
-  console.log(this);  
-};
-
-
-function populate (){
-  if(quiz.isEnded()){
-       showScores();
-  }
-  else {
-      //show question();
-      var element= document.getElementById("question");
-      element.innerHTML=quiz.getQuestionIndex().text;
-     clearInterval(time);
-      countdown();
-    
-      
-  
     
 
-      //show choice
-      var choices=quiz.getQuestionIndex().choices;
-      for(var i=0; i<choices.length; i++){
-          var element= document. getElementById("choice" + i);
-          element.innerHTML=choices[i];
-          guess("btn" + i, choices[i]);
-      }   
-      
-  }
-};
-
-function guess(id,guess) {
-  var button= document.getElementById(id);
-  button.onclick=function(){
-      quiz.guess(guess);
-      populate();
+     function populate(){
+        timer = setInterval(countDown,1000);
+        $("#sub").html('<h2 id="counter">Time Remaining: 5 Seconds </h2>');
+        $("#sub").append("<h2>"+funTriviaQuestions[currentQuestion].question+"</h2>");
+        for (var i=0; i<funTriviaQuestions[currentQuestion].answers.length;i++){
+        $("#sub").append('<button class="clickedAnswer" id="button-'+i+'" data-name="'+
+        funTriviaQuestions[currentQuestion].answers[i]+'">'+funTriviaQuestions[currentQuestion].
+        answers[i]+'</button>');
+        }
+    };
     
-  }
 
-};
-
-
-function showScores (){
-  var gameOverhtml='<h1>Result</h1>'
-  gameOverhtml+="<h2 id='score'> Correct Answers:"+ quiz.score+"</h2>";
-  //gameOverhtml+="<h2 id='score'> Incorrect Answer:"+ quiz.score+"</h2>";
+    function countDown(){
+        counter--;
+        $("#counter").html(counter);
+        if (counter<=0){
+            console.log("times up");
+            timesup();
+        }
+    };
  
-  var element=document.getElementById("quiz");
-  element.innerHTML=gameOverhtml;
+   function  nextQuestion(){
+        counter=5;
+        $('#counter').html(counter);
+        currentQuestion++;
+        populate();
+    };
+   function  timesup(){
+        clearInterval(timer);
+        unanswered++;
+        $('#sub').html('<h2> Time is Up!</h2>');
+        $('#sub').append('<h3>The Correct Answer Is:'+funTriviaQuestions[currentQuestion].correctAnswer+'</h3>');
+        if(currentQuestion==funTriviaQuestions.length-1){
+            setTimeout(results,2*1000);
+        }else {
+            setTimeout(nextQuestion,2*1000);
+        }
+    };
+   function  results(){ 
+        clearInterval(timer);
+        $('#sub').html('<h2>Results:</h2>');
+        $('#sub').append("<h3>Correct:"+correct+"</h3>");
+        $('#sub').append("<h3>Incorrect:"+incorrect+"<h3>");
+        $('#sub').append("<h3>Unanswered:"+unanswered+"<h3>");
+        $('#sub').append("<button id='reset'>Reset</button>");
+    };
+ function clicked(e){
+        clearInterval(timer);
+        if($(e.target).data("name")==funTriviaQuestions[currentQuestion].correctAnswer){
+            console.log("yeaay");
+        correctlyAnswered();
+        } else {
+        IncorrectlyAnswered();
+        console.log("NAyeaay");
+    }
+   
+    };
+   function  correctlyAnswered(){
+        clearInterval(timer);
+        correct++;
+        $('#sub').html('<h2> Correct! </h2>');
+        if(currentQuestion==funTriviaQuestions.length-1){
+            setTimeout(results,3*1000);
+        }else {
+            setTimeout(nextQuestion,3*1000);
+        }
 
-};
-
-
-var questions=[
-  new funTriviaGameQuestion("What nation was bounced from the Organisation of American States in 1962?", ["Cuba","Canada","Mixico","Gana"],"Cuba"),
-  new funTriviaGameQuestion("What continent has the fewest flowering plants?",["Asia","Antarctica","Africa","Australia"],"Antarctica"),
-  new funTriviaGameQuestion("What country saw a world record 315 million voters turn out for elections on May 20, 1991? ",["India","Canada","Mixico","Ethiopia"],"India"),
-  new funTriviaGameQuestion("What national holiday in Mexico has picnickers munching chocolate coffins and sugar skulls? ",["The Day of the Dead","Walking Dead","Dead People","Day of the Dead"],"The Day of the Dead"),
-  new funTriviaGameQuestion("What was the first planet to be discovered using the telescope, in 1781?",["Mars","Earth","Venus","Uranus"],"Uranus")
-];
-
-//
-var quiz= new Quiz(questions);
-
-
-
-// //find selected answer
-// var answerContainer = answerContainers[questionIndex];
-// var selector = `input[name=question${questionIndex}]:clicked`;
-// var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-// function showResults(){
-
-//   // gather answer containers from our quiz
-//  var answerContainers = funTriviaGameQuestion.querySelectorAll('.buttons');
-
-//   // keep track of user's answers
-//   let correctAnswer = 0;
-
-//   // for each question...
-// funTriviaGameQuestion.forEach( (currentQuestion, questionIndex) => {
-
-//     // find selected answer
-//    var answerContainer = answerContainers[questionIndex];
-//    var selector = 'input[name=question'+questionIndex+']:checked';
-//    var userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-//     // if answer is correct
-//     if(userAnswer===currentQuestion.correctAnswer){
-//       // add to the number of correct answers
-//       correctAnswer++;
-
-//       // color the answers green
-//       answerContainers[questionIndex].style.color = 'lightgreen';
-//     }
-//     // if answer is wrong or blank
-//     else{
-//       // color the answers red
-//       answerContainers[questionIndex].style.color = 'red';
-//     }
-//   });
-
-//   // show number of correct answers out of total
-//   gameOverhtml+=innerHTML = correctAnswer
-// }
-
-// // //reset questions
-// // function resetQuiz() {
-// //   funTriviaGameQuestion = 0;
-// //   correctAnswers = 0;
-// //   hideScore();
-// // };
-
+    };
+   function IncorrectlyAnswered(){
+        clearInterval(timer);
+        incorrect++;
+        $('#sub').html('<h2> Wrong</h2>');
+        $('#sub').append('<h3>The Correct Answer Is:'+funTriviaQuestions[currentQuestion].correctAnswer+'</h3>');
+        if(currentQuestion==funTriviaQuestions.length-1){
+            setTimeout(results,2*1000);
+        }else {
+            setTimeout(gmae.nextQuestion,2*1000);
+        }
+    };
+   function  reset(){
+        currentQuestion=0;
+        counter=0;
+        counter=0;
+        correct=0;
+        incorrect=0;
+        unanswered=0;
+        populate();
+   };
